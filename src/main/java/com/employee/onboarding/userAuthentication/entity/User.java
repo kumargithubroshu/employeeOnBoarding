37f339fbe3c784ff02617895bfc2bae29b3c89ea
Roshan <Roshan@DESKTOP-61S9O1S>
2024-12-24 09:12:52 -0800

    added entity and validation dependency

---
+++ src/main/java/com/employee/onboarding/userAuthentication/entity/User.java
@@ -0,0 +1,60 @@
+package com.employee.onboarding.userAuthentication.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long userId;
+
+	@Column(name = "name")
+	private String userName;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "email")
+	@Email(regexp = "^[a-zA-Z0-9._%+-]+@gmail\\.com$", message = "Email must be a valid Gmail address")
+	private String email;
+
+	@Column(name = "role")
+	private String role;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@Column(name = "phone_number")
+	private String phoneNumber;
+
+	@Column(name = "status")
+	private String status;
+
+	@Column(name = "role_description")
+	private String description;
+
+}
